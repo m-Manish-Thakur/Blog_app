2ef8a50de0be19141961876cbfa9058e1445b97e
Manish Thakur <manishthakur231690@gmail.com>
2023-10-12 19:38:31 +0530

    Added

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ const cors = require("cors");
 
 app.use(cors());
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
